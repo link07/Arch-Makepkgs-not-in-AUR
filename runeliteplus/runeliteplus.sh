62d0dfba435d77d212a047eb0cd65466d574b376
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2019-08-29 10:54:50 -0600

    Moved runeliteplus to aur

--- runeliteplus/runeliteplus.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# jagex pollutes ~/ with a few files (jagexcache, jagex_cl_oldschool_LIVE.dat, etc), so we'll move it to its own folder
-exec /usr/lib/jvm/java-8-openjdk/jre/bin/java -Dhttps.protocols=TLSv1.2 -Duser.home="$HOME/.runescape" -jar /usr/share/runeliteplus/RuneLite.jar "$@"
